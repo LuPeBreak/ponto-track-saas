7c82294da2cf951b762bcb4d304c8a15cfcf098d
LuPeBreak <lfbmrj15@gmail.com>
2024-06-27 23:03:51 -0300

    Update: - Created route authenticate with password - Created route authenticate with GitHub - Created route get profile - Created route request password recover - Created route reset password - Implemented error handler - Created BadRequestError and UnauthorizedError error classes - Created env packed - Implemented environment variables

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,8 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
+import { env } from '@ponto-track-saas/env'
 import { fastify } from 'fastify'
 import {
   jsonSchemaTransform,
@@ -9,11 +11,21 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { errorHandler } from './error-handler'
+// Routes
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
+import { getProfile } from './routes/auth/get-profile'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
+
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
 
 app.register(fastifySwagger, {
   openapi: {
@@ -23,17 +35,36 @@ app.register(fastifySwagger, {
         'A simple full-stack SaaS app with multi-tenant & RBAC, made to as a test for a full-stack web developer job position',
       version: '1.0.0',
     },
-    servers: [],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   transform: jsonSchemaTransform,
 })
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
-app.register(fastifySwaggerUI, { routePrefix: '/docs' })
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(fastifyCors)
 
+// Routes
 app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
+app.register(getProfile)
+app.register(requestPasswordRecover)
+app.register(resetPassword)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is Running!')
 })
