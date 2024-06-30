7f82ef201c76d74ba92610f75effadccc4718c36
LuPeBreak <lfbmrj15@gmail.com>
2024-06-30 10:01:38 -0300

    Update: Middleware save org & organizationSwitcher show current - Created Middleware to save current org in cookies - Changed organization switcher to show current organization - Changed profile avatar size - Prettier fixes on ui components

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/header'
+
+export default async function Vehicles() {
+  return (
+    <div className="py-4">
+      <Header />
+      <main></main>
+    </div>
+  )
+}
